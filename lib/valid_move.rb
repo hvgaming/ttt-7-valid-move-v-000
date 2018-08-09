b6820dae069a98044d81a6dd27380b51312d0519
hugo valois <hvgaming@yahoo.com>
2018-08-09 22:17:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,9 @@
 # code your #valid_move? method here
 def valid_move?
-  
+  if position_taken?
+    return true
+  else
+    return 
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
